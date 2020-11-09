15eba32f991c66da4f7e00f48a87b61dc998c84c
Attednev <damian.gasser@gmail.com>
2020-11-09 09:23:43 +0100

    Added client side

--- src/com/company/Client/ClientMain.java
+++ src/com/company/Client/ClientMain.java
@@ -1,8 +1,21 @@
 package com.company.Client;
 
+import com.company.RemoteInterface.MyRMIInterface;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class ClientMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
+        MyRMIInterface serverLookUp = (MyRMIInterface) Naming.lookup("//localhost/MyServer");
+
+        int a = 10;
+        int b = 5;
+        int response = serverLookUp.addInt(10, 5);
+        System.out.println("Server response for 'addInt(" + a + ", " + b + ")' is: " + response);
 
     }
 }
